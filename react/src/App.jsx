c3cbabf42ba286fa39435a341ee377fca4ffd07d
Sachan-Vartika <vartiika14@gmail.com>
2026-08-17 10:27:12 +0530

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Student from './Components/Student'
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{color:'greenyellow'}}>VARTIKA SACHAN</h1>
+    <Student/>
+    <br/>
+    <Student/>
+    <br/>
+    <Student/>
+    <br/>
+    </div>
+  )
+}
+
+export default App
